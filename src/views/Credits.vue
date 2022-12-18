0d256c1377621dbfdb4c6e6e6b72708663a6ad98
phntmz_ <hieuminhnguyen1273@gmail.com>
2022-12-18 22:32:05 +0700

    working on backend, not yet functional

--- src/views/Credits.vue
+++ src/views/Credits.vue
@@ -1,53 +1,22 @@
 <script>
+import axios from "axios";
 export default {
     data() {
         return {
-            credits: [
-                {
-                    roles: "inspired by",
-                    people: [
-                        "<a href='https://weare.tm/' target='_blank'>aristide benoist's tm</a>",
-                    ],
-                },
-                {
-                    roles: "photography",
-                    people: ["bao anh ngo", "minh anh do", "hieu minh nguyen"],
-                },
-                {
-                    roles: "lighting",
-                    people: ["bao anh ngo", "minh anh do", "hieu minh nguyen"],
-                },
-                {
-                    roles: "literary // language",
-                    people: ["bao anh ngo", " minh anh do", "hieu minh nguyen"],
-                },
-                {
-                    roles: "user interface // experience",
-                    people: ["hieu minh nguyen", "ayush pramanik"],
-                },
-                {
-                    roles: "motion",
-                    people: ["hieu minh nguyen"],
-                },
-                {
-                    roles: "backend development",
-                    people: ["hieu minh nguyen"],
-                },
-                {
-                    roles: "faces",
-                    people: [
-                        "bao anh ngo",
-                        "huong giang vu",
-                        "jamiel masangkay",
-                        "cong minh ho",
-                        "hieu minh nguyen",
-                        "dan anh do",
-                        "minh anh do",
-                        "anh thu le",
-                    ],
-                },
-            ],
+            credits: [],
         };
+    },
+    created() {
+        let apiURL = "web-production-c75c.up.railway.app/api/credit";
+        axios
+            .get(apiURL)
+            .then((res) => {
+                this.credits = res.data;
+                console.log(res.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     },
 };
 </script>
@@ -58,7 +27,10 @@ export default {
             <h1>credits</h1>
         </div>
         <div class="col-start-2 col-span-1">
-            <div class="grid grid-cols-2 gap-24 mb-8" v-for="item in credits">
+            <div
+                class="grid grid-cols-2 gap-24 mb-8"
+                v-for="item in credits.data.data.slice().reverse()"
+            >
                 <div class="col-span-1 text-sm">
                     {{ item.roles }}
                 </div>
